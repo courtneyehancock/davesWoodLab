d1a7a2a6509a66166b1d2e184826a5d1ff5ab7ca
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2019-12-03 16:13:35 -0500

    Adjust functions on homepage

--- page-home.php
+++ page-home.php
@@ -18,53 +18,17 @@
 </div>
 
 <div class="mt-5">
-  <div class="row align-items-center">
-    <div class="col-md-6 text-center pl-5">
-      <!-- About Brief Text Widget-->
-      <?php dynamic_sidebar('about-brief-home'); ?>
-    </div>
-    <div class="col-md-6">
-      <!--Img Widget-->
-      <?php dynamic_sidebar('right-img-home'); ?>
-    </div>
-  </div>
-</div>
-
-<div>
-  <div class="row align-items-center">
-    <div class="col-md-6">
-      <!-- Left Img Widget-->
-      <?php dynamic_sidebar('left-img-home'); ?>
-    </div>
-    <div class="col-md-6 text-center pr-5">
-      <!--Right About Brief Widget-->
-      <?php dynamic_sidebar('right-about-brief-home'); ?>
-    </div>
-  </div>
-</div>
-
-<div>
-  <div class="row align-items-center">
-    <div class="col-md-6 text-center pl-5">
-      <!--About Brief Text Widget-->
-      <?php dynamic_sidebar('about-brief-home-two'); ?>
-    </div>
-    <div class="col-md-6">
-      <!--Img Widget 2-->
-      <?php dynamic_sidebar('right-img-home-two'); ?>
-    </div>
-  </div>
-</div>
-
-<div class="mb-5">
-  <div class="row align-items-center">
-    <div class="col-md-6">
-      <!-- Left Img Widget-->
-      <?php dynamic_sidebar('left-img-home-two'); ?>
-    </div>
-    <div class="col-md-6 text-center pr-5">
-      <!--About Right Brief Widget Widget-->
-      <?php dynamic_sidebar('right-about-brief-home-two'); ?>
+  <div class="row">
+    <div class="col-md-12">
+      <!--Text-->
+      <?php
+        if(have_posts()){
+          while(have_posts()){
+            the_post();?>
+            <?php the_content();
+          }
+        }
+      ?>
     </div>
   </div>
 </div>
