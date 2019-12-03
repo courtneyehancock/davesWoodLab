34f284b60311841cd8dddf59950498f6461c8e2b
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2019-12-03 10:11:25 -0500

    Edit footer contents

--- footer.php
+++ footer.php
@@ -1,13 +1,43 @@
 <footer>
   <div class="container">
+    <div class="row">
+      <div class="col-md-5">
+        <!--Logo/text widget-->
+        <?php dynamic_sidebar('left-footer'); ?>
+      </div>
 
-    <div>
-      <img class="footer-img" src="http://courtneyehancock.com/legacyhospitalityllc/wp-content/uploads/2019/11/legacy_footer.png" alt="footer image" />
+      <div class="col-md-1">
+        <!--Middle Left Space-->
+        <?php dynamic_sidebar('middle-left-footer'); ?>
+      </div>
+
+      <div class="col-md-3">
+        <!--Menu Title Widget-->
+        <?php dynamic_sidebar('menu-title-footer'); ?>
+        <!-- Navigation -->
+        <?php wp_nav_menu(array(
+          'theme_location' => 'footer-menu',
+          'container-class' => 'menu-footer'
+          ));
+        ?>
+      </div>
+
+      <div class="col-md-3">
+        <!--Contact Widget-->
+        <?php dynamic_sidebar('right-footer'); ?>
+        <!--Social Widget-->
+        <?php dynamic_sidebar('social-footer'); ?>
+      </div>
     </div>
 
+    <div class="row">
+      <div class="col-md-12">
+        <!--Copyright-->
+        <p class="center-text footer-end">&copy; Site created by Courtney Hancock 2019</p>
+      </div>
+    </div>
   </div>
 </footer>
-
 
 <?php wp_footer(); ?>
 
