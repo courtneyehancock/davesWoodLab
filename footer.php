7762abab78555b796000dd3cc2e4e1d542850a9b
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2021-10-25 14:52:39 -0400

    Center footer

--- footer.php
+++ footer.php
@@ -1,16 +1,7 @@
 <footer>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-4">
+  <div class="container d-flex justify-content-center">
         <!--Logo/text widget-->
         <?php dynamic_sidebar('left-footer'); ?>
-      </div>
-
-      <div class="col-md-8">
-        <!--Contact Widget-->
-        <?php dynamic_sidebar('right-footer'); ?>
-      </div>
-    </div>
 
         <!--Copyright-->
         <p class="text-center"> © <?php echo date("Y"); ?> Dave's Wood Lab </p>
