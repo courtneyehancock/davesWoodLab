9ca0b28512be6a1de59e5569a8674fb343d967e4
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2021-10-25 14:39:38 -0400

    Edit footer

--- footer.php
+++ footer.php
@@ -1,39 +1,26 @@
 <footer>
   <div class="container">
     <div class="row">
-      <div class="col-md-5">
+      <div class="col-md-4">
         <!--Logo/text widget-->
         <?php dynamic_sidebar('left-footer'); ?>
       </div>
 
-      <div class="col-md-1">
+      <div class="col-md-4">
         <!--Middle Left Space-->
         <?php dynamic_sidebar('middle-left-footer'); ?>
       </div>
 
-      <div class="col-md-3">
-        <!--Menu Title Widget-->
-        <?php dynamic_sidebar('menu-title-footer'); ?>
-        <!-- Navigation -->
-        <?php wp_nav_menu(array(
-          'theme_location' => 'footer-menu',
-          'container-class' => 'menu-footer'
-          ));
-        ?>
-      </div>
-
-      <div class="col-md-3">
+      <div class="col-md-4">
         <!--Contact Widget-->
         <?php dynamic_sidebar('right-footer'); ?>
-        <!--Social Widget-->
-        <?php dynamic_sidebar('social-footer'); ?>
       </div>
     </div>
 
     <div class="row">
       <div class="col-md-12">
         <!--Copyright-->
-        <p class="text-center font-italic">&copy; Site created by Courtney Hancock 2019</p>
+        <p> © <?php echo date("Y"); ?> Dave's Wood Lab </p>
       </div>
     </div>
   </div>
