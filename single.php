9890f151f8afc3fe4c0609f86ae60c0d1564deab
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2021-10-25 14:24:22 -0400

    Edit single post

--- single.php
+++ single.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 
-<div class=container>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-5 p-4 m-2">
 <?php
   if(have_posts()){
     while(have_posts()){
@@ -8,26 +10,17 @@
       <div class="post-feature">
         <?php the_post_thumbnail(); ?>
       </div>
+    </div>
+    <div class="col-lg-5 p-4 m-2">
       <h2 class="post-title"><?php the_title(); ?></h2>
-      <p><?php echo "Published: " . get_the_date(); ?></p>
       <?php the_content(); ?>
-      <p><?php echo "This post is written by : " . get_the_author(); ?></p>
 
-      <div class="pagination-single">
-        <?php previous_post_link('%link', 'Previous Post >>');?>
-      </div>
-
-      <div class="pagination-single">
-        <?php next_post_link('%link', 'Next Post >>');?>
-      </div>
 
     </div>
+  </div>
       <?php
     }
   }
-  if (comments_open() || get_comments_number()):
-    comments_template();
-  endif;
 ?>
 </div>
 
